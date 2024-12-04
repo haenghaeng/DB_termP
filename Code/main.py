c4bc934694f3e2edc7b68c1ba3ca2930edcc6530
haenghaeng <haeng@pusan.ac.kr>
2024-12-04 11:57:26 +0900

    refactor : main
    1. 기존의 예제 테이블을 참조하는 코드에서 현재 프로젝트에 대응하는 테이블을 참조하는 코드로 변경하였습니다.
    2. SQL injection 방지를 위해 쿼리문 실행 로직을 변경하였습니다.

--- Code/main.py
+++ Code/main.py
@@ -1,54 +1,56 @@
 import psycopg2
-import hashlib
-
-def hash_password(password):
-    # SHA-256 해시 객체 생성
-    sha256_hash = hashlib.sha256()
-    
-    # 비밀번호를 바이트로 변환 후 해싱
-    sha256_hash.update(password.encode('utf-8'))
-    
-    # 해시값을 16진수로 반환
-    return sha256_hash.hexdigest()
+import psycopg2.sql
+import hashpw
 
 # 입력 받은 유저명과 비밀번호를 DB에서 확인합니다. 일치하면 True, 일치하지 않으면 False를 리턴합니다.
 def try_connection(user_name, user_password):
-    connection = psycopg2.connect(
-        database = "db_termp",
-        user = 'admin',
-        password = 'admin',
-        host = "::1",
-        port = "5432"
-        )
-    
-    cursor = connection.cursor()
-    
-    cursor.execute(
-        f'''
-        select user_name
-        from users u
-        where u.user_name = '{user_name}';
-        '''
-    )
-    
-    if len(cursor.fetchall()) > 0:
-        cursor.execute(
-            f'''
-            select *
-            from users u
-            where u.user_name = '{user_name}' and u.password = '{hash_password(user_password)}';
-            '''
-        )
+    try:        
+        connection = psycopg2.connect(
+            database = "db_termp",
+            user = 'login',
+            password = 'login',
+            host = "::1",
+            port = "5432"
+            )
         
-        if len(cursor.fetchall()) > 0:
-            return True
+        cursor = connection.cursor()
+        
+        # 입력값과 SQL 구문을 분리해야 SQL injection 위험이 없다고 하네용
+        query = psycopg2.sql.SQL(
+            '''
+            select user_name
+            from id_pw_view ipv 
+            where ipv.user_name = %s;
+            '''
+        )    
+        cursor.execute(query, (user_name,)) # 전달할 땐 tuple로 보내야 함
+        
+        if cursor.fetchone():            
+            query = psycopg2.sql.SQL(
+                '''
+                select user_name, user_password
+                from id_pw_view ipv
+                where ipv.user_name = %s and ipv.user_password = %s;
+                '''
+            )            
+            cursor.execute(query, (user_name, hashpw.hash_password(user_password)))
+            
+            # 결과가 존재하면 True
+            if cursor.fetchone() :
+                return True
+            else:
+                print("\n비밀번호가 틀렸습니다.")
+                return False  
         else:
-            print("비밀번호가 틀렸습니다.")
-            return False  
-    else:
-        print("사용자가 존재하지 않습니다.")
+            print("\n사용자가 존재하지 않습니다.")
+            return False
+        
+    except Exception as e:
+        print(f"\n데이터베이스와 연결에 실패하였습니다. : {e}")
         return False
-
+    
+    finally:
+        connection.close()
 
 # 사용자에게서 사용자명과 비밀번호를 받아 연결을 시도합니다.
 def login():  
@@ -56,16 +58,20 @@ def login():
     user_password = input("비밀번호를 입력해주세요 ")
 
     if(try_connection(user_name, user_password)):
-        print("로그인 성공")
+        print("\n로그인 성공")
     else:
-        print("로그인 실패")
+        print("\n로그인 실패")
 
-if __name__ == '__main__':   
+
+def main():
     while True:
         action = input("\n원하는 기능의 숫자를 입력해주세요 \n(1) 로그인\n(2) 종료\n")
-        
+                
         if action == "1":
             login()
-        if action == "2":
+        elif action == "2":
             print("종료합니다.")
             break
+
+if __name__ == '__main__':   
+    main()
