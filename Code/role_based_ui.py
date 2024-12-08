15341e1f26bf6e33557ff4c801451f2593928ea7
haenghaeng <haeng@pusan.ac.kr>
2024-12-08 20:13:48 +0900

    fix : role_based_ui
    코드가 일부 누락되어 다시 올립니다.

--- Code/role_based_ui.py
+++ Code/role_based_ui.py
@@ -4,6 +4,9 @@ import wired_equipment
 import wireless_equipment
 import computer_equipment
 
+import admin_incident_reports
+import incident_reports
+
 def login(department):    
     if department == 'wireless_operator':
         print('무선반 접속')
@@ -23,6 +26,7 @@ def login(department):
     
     elif department == 'etc':
         print('기타 접속')
+        etc_ui()
  
 def wireless_operator_ui(department):
     while True:
@@ -54,7 +58,7 @@ def computer_technician_ui(department):
 
 def admin_ui(department):
     while True:
-        action = input('\n원하는 기능의 숫자를 입력해주세요 \n(1) 병사 관리\n(2) 무선 장비 관리\n(3) 유선 장비 관리\n(4) 전자 장비 관리\n(5) 종료\n')
+        action = input('\n원하는 기능의 숫자를 입력해주세요 \n(1) 병사 관리\n(2) 무선 장비 관리\n(3) 유선 장비 관리\n(4) 전자 장비 관리\n(5) 사고 관리\n(6) 종료\n')
         if action == "1":
             soldier_information_table.main()
         elif action == "2":
@@ -64,5 +68,16 @@ def admin_ui(department):
         elif action == "4":
             computer_equipment.main(department)
         elif action == "5":
+            admin_incident_reports.main(department)
+        elif action == "6":
+            print("종료합니다.")
+            break
+        
+def etc_ui():
+    while True:
+        action = input('\n원하는 기능의 숫자를 입력해주세요 \n(1) 사고 등록\n(2) 종료\n')
+        if action == "1":
+            incident_reports.main()
+        elif action == "2":
             print("종료합니다.")
             break
